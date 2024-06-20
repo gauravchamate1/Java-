dd82e64a2609a13d40abf901defa26d5182076c8
Gaurav Chamate <40346407+gauravchamate1@users.noreply.github.com>
2024-06-20 19:52:40 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,17 @@
+package com.identifiers.operator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a1;
+int b1;
+int res;
+
+a1=50;
+b1=40;
+res=a1+b1;
+System.out.println(res);
+	}
+
+}
