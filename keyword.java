c04175f9118223369fa9720dd683a69a6b8c3016
Gaurav Chamate <40346407+gauravchamate1@users.noreply.github.com>
2024-06-20 19:54:13 +0530

    Create keyword.java
    .

---
+++ keyword.java
@@ -0,0 +1,94 @@
+keywords
+
+blue-keywords
+red-inbuilt class
+black -identifier
+
+
+1.abstract 
+2.assert
+3.boolean
+4.break
+5.byte
+6.case
+7.catch
+8.char
+9.class
+10.const
+11.continue
+12.default
+13.double
+14.enum
+15.extends
+16.finally
+17.throws
+18.   
+19,float
+20.finalize
+21.try
+22.void
+23.volatile
+24.for
+25.goto
+26.static
+27.import
+
+Identifers
+
+names which are given to classes and interfaces
+variables
+methods to identify them unquely
+
+rules of identifers
+
+alphanumeric values
+
+should not start with no 
+$,_are the only special cahractrs
+special character
+
+
+naming convention
+
+classname/interfacename----BankAccount
+variables--lowercase ---height
+writing a method--writeExam()
+constants--PI
+
+varaibale
+
+varaibles are those memory locations which are used to store the value and identifed by names
+
+
+
+to use variable in java 3steps
+
+declaration
+initialization
+utilization
+
+decaratio n-it is statement which tells compiler what type
+ of data i need to store in the variable and comiler allocates memory according to dattype
+
+int i ;
+
+datatype  capacity
+
+
+byte        8bits
+short       16bits
+int         32
+long         64
+float        32
+double        64
+boolean     8
+char        16bits
+
+
+initaialization
+
+statement written to store the value in varaibale
+
+var name=value;
+
+int a=20;
